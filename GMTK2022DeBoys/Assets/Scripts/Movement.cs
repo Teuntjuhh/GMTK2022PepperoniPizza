4d8b0c964c54754c224dfc1f2607d440c53b59d9
KaSien <lieuwian@gmail.com>
2022-07-15 23:31:06 +0200

    MovementDraft

---
+++ GMTK2022DeBoys/Assets/Scripts/Movement.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 inputDirection;
+    public float rotationSpeed;
+
+    public float horizontalInput;
+    public float verticalInput;
+    public float inputTimeH;
+    public float inputTimeV;
+
+    public bool isMoving;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputListener();
+        TimeFactor();
+        if (!isMoving && inputDirection != Vector3.zero)
+        {
+            Move(inputDirection);
+        }
+    }
+
+    void Move(Vector3 direction)
+    {
+        var anchor = transform.position + (Vector3.down + direction) * 0.5f;
+        var axis = Vector3.Cross(Vector3.up, direction);
+        StartCoroutine(DiceRoll(anchor, axis));
+    }
+
+    IEnumerator DiceRoll(Vector3 anchor, Vector3 axis)
+    {
+        isMoving = true;
+
+        for(int i=0; i<(90/rotationSpeed); i++)
+        {
+            transform.RotateAround(anchor, axis, rotationSpeed);
+            yield return new WaitForSeconds(0.01f);
+        }
+        transform.position = new Vector3(Mathf.Round(transform.position.x), 0.5f, Mathf.Round(transform.position.z));
+        isMoving = false;
+    }
+    
+    Vector3 GetInputDirection()
+    {
+        if(horizontalInput != 0 && verticalInput != 0)
+        {
+            if(inputTimeH > inputTimeV)
+            {
+                return inputDirection = new Vector3(horizontalInput, 0, 0);
+            }
+            else
+            {
+                return inputDirection = new Vector3(0, 0, verticalInput);
+            }
+        }
+        else
+        {
+            return inputDirection = new Vector3(horizontalInput, 0, verticalInput);
+        }
+    }
+
+    void InputListener()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        GetInputDirection();
+    }
+
+    void TimeFactor()
+    {
+        if(horizontalInput != 0)
+        {
+            inputTimeH += Time.deltaTime;
+        }
+        else
+        {
+            inputTimeH = 0;
+        }
+
+        if(verticalInput != 0)
+        {
+            inputTimeV += Time.deltaTime;
+        }
+        else
+        {
+            inputTimeV = 0;
+        }
+    }
+}
