4a70ab541ea403b82ac87d7b567f39b1fae6d9a4
L.L <galleonidas@gmail.com>
2022-07-16 01:51:10 +0200

    Added Button Controls

---
+++ GMTK2022DeBoys/Assets/NrTwoScissors.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NrTwoScissors : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha2))
+        {
+            animator.SetTrigger("IsPressingTwo");
+        }
+        else
+        {
+            animator.ResetTrigger("IsPressingTwo");
+        }
+    }
+
+}
