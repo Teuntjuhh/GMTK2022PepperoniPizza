fa3e9f0b6c5e08a0bb169e84bb365e29d60861aa
Teun van den Broek <teunvdbroek@gmail.com>
2022-07-15 20:52:50 +0200

    Added Basic Pawn Movement

---
+++ GMTK2022DeBoys/Assets/Scripts/Enemies/Pawn.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class Pawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    private float moveDelay = 1;
+    private float timeSinceLastMove = 0;
+
+    private float moveDuration = 0.25f;
+
+    private void Update()
+    {
+        timeSinceLastMove += Time.deltaTime;
+
+        if(timeSinceLastMove >= moveDelay)
+        {
+            MoveTile();
+            timeSinceLastMove = 0;
+        }
+    }
+
+    private void MoveTile()
+    {
+        List<Direction> directionsToGo = new List<Direction>();
+
+        if((Mathf.Round(transform.position.z) > Mathf.Round(player.transform.position.z)))
+        {
+            directionsToGo.Add(Direction.Down);
+        }
+
+        else if ((Mathf.Round(transform.position.z) < Mathf.Round(player.transform.position.z)))
+        {
+            directionsToGo.Add(Direction.Up);
+        }
+
+        if ((Mathf.Round(transform.position.x) > Mathf.Round(player.transform.position.x)))
+        {
+            directionsToGo.Add(Direction.Left);
+        }
+
+        else if ((Mathf.Round(transform.position.x) < Mathf.Round(player.transform.position.x)))
+        {
+            directionsToGo.Add(Direction.Right);
+        }
+
+        StartCoroutine(MoveAnimation(directionsToGo[Random.Range(0, directionsToGo.Count)]));
+    }
+
+    private IEnumerator MoveAnimation(Direction direction)
+    {
+        Vector3 targetPosition = new Vector3();
+
+        switch (direction)
+        {
+            case Direction.Up:
+                targetPosition = transform.position + new Vector3(0, 0, 1);
+                break;
+            case Direction.Down:
+                targetPosition = transform.position + new Vector3(0, 0, -1);
+                break;
+            case Direction.Left:
+                targetPosition = transform.position + new Vector3(-1, 0, 0);
+                break;
+            case Direction.Right:
+                targetPosition = transform.position + new Vector3(1, 0, 0);
+                break;
+            default:
+                break;
+        }
+
+        float currentDuration = 0;
+        Vector3 startingPosition = transform.position;
+
+        while (currentDuration < moveDuration)
+        {
+            currentDuration += Time.deltaTime;
+            transform.position = Vector3.Slerp(startingPosition, targetPosition, currentDuration / moveDuration);
+            yield return null;
+        }
+    }
+
+    private void DamagePlayer()
+    {
+        //TODO
+    }
+}
