f71f15c5ce99c66ab26af887e36014e074bc31e2
L.L <galleonidas@gmail.com>
2022-07-17 19:10:29 +0200

    Added ScissorSwitch

--- GMTK2022DeBoys/Assets/Scripts/SceneSwitching.cs
+++ GMTK2022DeBoys/Assets/Scripts/SceneSwitching.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitching : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
     public void SwitchScene(string name)
     {
         SceneManager.LoadScene(sceneName:name);
